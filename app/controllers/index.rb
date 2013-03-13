d52a4ffa77f51e72c557ead9c0682f27fdb66c97
Apprentice <apprentice@devbootcamp.com>
2013-03-13 11:39:22 -0700

    short url get route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,8 +4,19 @@ get '/' do
 end
 
 post '/urls' do
-  @urls = Url.all
+  @urls = Url.all #TODO: make this order in a sane way
   @url = Url.new :long_url => params[:long_url]
   @url.save
   redirect '/'
+end
+
+get '/:short_url' do
+  @url = Url.find_by_short_url(params[:short_url])
+  @new_count = (@url.click_count + 1)
+  @url.update_attribute :click_count, @new_count
+  # puts "%%%"* 40
+  # @url.update_attributes :click_count, params[:click_count] += 1
+  # p "-"*100
+  # p params[:click_count] += 1
+  redirect "#{@url.long_url}"
 end
